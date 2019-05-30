55edb8b943197711e468f3f01c8ace051a819758
TLI - Sushmita (SY) <sushmita@tli.com>
2019-05-30 01:21:24 -0400

    readme info- initial

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
 CREATE TABLE burgers 
 (
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(100),
 devoured BOOLEAN,
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
 );
